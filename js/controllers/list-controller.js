71d67c8724b8d354dfc3d201283c5d65c54990e9
Gustavo Lucena <gustavolucena12@gmail.com>
2023-02-13 20:19:52 -0300

    add: button - remove element

--- js/controllers/list-controller.js
+++ js/controllers/list-controller.js
@@ -13,13 +13,36 @@ export function addCard(address){
     state.listSection.appendChild(card);
 }
 
+function handleTrashClick(event){
+    event.preventDefault();
+    const element = event.target.parentElement.parentElement;
+    removeCard(element);
+}
+
+function removeCard(element){
+    element.remove();
+}
+
 function createCard(address){
+
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("card-list-container")
 
     const div = document.createElement("div");
     div.classList.add("card-list-item");
 
+    const divTrash = document.createElement("div");
+    divTrash.classList.add("card-list-trash");
+
     const h3 = document.createElement("h3");
     h3.innerHTML = address.city;
+
+    const i = document.createElement("i");
+    i.classList.add("fa");
+    i.classList.add("fa-trash-o");
+    i.style.fontSize = "26px";
+
+    i.addEventListener('click', handleTrashClick);
 
     const line = document.createElement("p");
     line.classList.add("address-line");
@@ -29,9 +52,14 @@ function createCard(address){
     cep.classList.add("address-cep");
     cep.innerHTML = address.cep;
 
+    divContainer.appendChild(div);
+    divContainer.appendChild(divTrash);
+
     div.appendChild(h3);
     div.appendChild(line);
     div.appendChild(cep);
 
-    return div;
+    divTrash.appendChild(i);
+
+    return divContainer;
 }
